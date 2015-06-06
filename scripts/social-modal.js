a8754c5b4d471f1971512d179b1d7688a2dc9cfd
Anthony Williams <92antwilliams@gmail.com>
2015-06-06 18:16:27 +0100

    Refactored js

--- scripts/social-modal.js
+++ scripts/social-modal.js
@@ -1,13 +1,21 @@
 $(function() {
     $( '.social-modal-activator' ).click(function() {
-        $( '.social-modal' ).animate({top: "0", opacity: "1"}, 200);
+        showModal();
     });
     $( '.social-modal .close-button' ).click(function() {
-        $( '.social-modal' ).animate({top: "-100%", opacity: "0"}, 100);
+        hideModal();
     });
     $(document).keyup(function(e) {
         if (e.which == 27) {
-            $( '.social-modal' ).animate({top: "-100%", opacity: "0"}, 100);
+            hideModal();
         }
     });
 });
+
+var showModal = function() {
+    $( '.social-modal' ).animate({top: "0", opacity: "1"}, 200);
+}
+
+var hideModal = function() {
+    $( '.social-modal' ).animate({top: "-100%", opacity: "0"}, 100);
+}
