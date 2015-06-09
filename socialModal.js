9a155c4208081b4a6f0a8fc94866c127bef4874e
Anthony Williams <92antwilliams@gmail.com>
2015-06-09 17:13:22 +0100

    Now supports multiple modals on a page, more examples

--- socialModal.js
+++ socialModal.js
@@ -1,21 +1,76 @@
 (function($) {
 
     $.fn.socialModal = function(options) {
-        opts = $.extend(true, {}, $.fn.socialModal.defaults, options);
-        openButton = this;
-        injectHTML();
-        attachStyles();
-        addOpenEvents();
-        addCloseEvents();
+        // get hold of options
+        var opts = $.extend(true, {}, $.fn.socialModal.defaults, options);
+        if (!opts.modal.showCss) { opts.modal.showCss = { "top": 0, "opacity": 1 } }
+        if (!opts.modal.hideCss) { opts.modal.hideCss = { "top": "-100%", "opacity": 0 } }
+
+        // Create Modal and Buttons
+        var modal = $("<div />").addClass("socialModal").appendTo("body");
+        var closeButton = $("<button />").addClass("close-button").text(opts.closeButton.text).appendTo(modal);
+        var buttonContainer = $("<div />").addClass("button-container").appendTo(modal);
+
+        var facebookButton; var twitterButton; var redditButton; var gplusButton; var pinterestButton;
+        if (opts.facebook.enabled) {
+        facebookButton = $("<button />").addClass("share-facebook").text(opts.facebook.text)
+            .click(function() { facebookClickHandler(opts); }).appendTo(buttonContainer);
+        }
+        if (opts.twitter.enabled) {
+        twitterButton = $("<button />").addClass("share-twitter").text(opts.twitter.text)
+            .click(function() { twitterClickHandler(opts); }).appendTo(buttonContainer);
+        }
+        if (opts.reddit.enabled) {
+        redditButton = $("<button />").addClass("share-reddit").text(opts.reddit.text)
+            .click(function() { redditClickHandler(opts); }).appendTo(buttonContainer);
+        }
+        if (opts.gplus.enabled) {
+        gplusButton = $("<button />").addClass("share-gplus").text(opts.gplus.text)
+            .click(function() { gplusClickHandler(opts); }).appendTo(buttonContainer);
+        }
+        if (opts.pinterest.enabled) {
+        pinterestButton = $("<button />").addClass("share-pinterest").text(opts.pinterest.text)
+            .click(function() { pinterestClickHandler(opts); }).appendTo(buttonContainer);
+        }
+
+        // add icons
+        if (opts.useIcons) {
+            closeButton.addClass(opts.closeButton.iconClass);
+            if (facebookButton) { facebookButton.addClass(opts.facebook.iconClass); }
+            if (twitterButton) { twitterButton.addClass(opts.twitter.iconClass); }
+            if (redditButton) { redditButton.addClass(opts.reddit.iconClass); }
+            if (gplusButton) { gplusButton.addClass(opts.gplus.iconClass); }
+            if (pinterestButton) { pinterestButton.addClass(opts.pinterest.iconClass); }
+        };
+
+        // Hide modal to begin with
+        modal.css( { "position": "fixed", "width": "100%", "height": "100%", "z-index": 9999 } );
+        modal.css(opts.modal.hideCss);
+
+        // Trigger open
+        this.click(function() {
+            modal.animate(opts.modal.showCss, opts.modal.showAnimationDuration);
+        });
+
+        // Trigger close
+        closeButton.click(function() {
+            modal.animate(opts.modal.hideCss, opts.modal.hideAnimationDuration);
+        });
+
+        $(document).keyup(function(e) {
+            if (e.which == 27) {
+                modal.animate(opts.modal.hideCss, opts.modal.hideAnimationDuration);
+            }
+        });
     };
 
     $.fn.socialModal.defaults = {
         useIcons: true,
         url: window.location.href,
         modal: {
-            showCss: { "top": 0, "opacity": 1 },
+            showCss: null, // defaults to { "top": 0, "opacity": 1 } if unset
             showAnimationDuration: 200,
-            hideCss: { "top": "-100%", "opacity": 0 },
+            hideCss: null, // defaults to { "top": "-100%", "opacity": 0 } if unset
             hideAnimationDuration: 100,
         },
         closeButton: {
@@ -53,100 +108,14 @@
         },
     };
 
-    // Private variables
-
-    var opts;
-    var modal;
-    var closeButton;
-    var openButton; // may be a collection of elements that may not be buttons
-    var buttonContainer;
-    var facebookButton;
-    var twitterButton;
-    var redditButton;
-    var gplusButton;
-    var pinterestButton;
-
-    // Private functions
-
-    var injectHTML = function() {
-        modal = $("<div />").addClass("socialModal").appendTo("body");
-        addCloseButton();
-        buttonContainer = $("<div />").addClass("button-container").appendTo(modal);
-        !opts.facebook.enabled || addFacebookButton();
-        !opts.twitter.enabled || addTwitterButton();
-        !opts.reddit.enabled || addRedditButton();
-        !opts.gplus.enabled || addGPlusButton();
-        !opts.pinterest.enabled || addPinterestButton();
-        !opts.useIcons || addIconClasses();
-    };
-
-    var addCloseButton = function() {
-        closeButton = $("<button />").addClass("close-button").text(opts.closeButton.text).appendTo(modal);
-    };
-
-    var addFacebookButton = function() {
-    facebookButton = $("<button />").addClass("share-facebook").text(opts.facebook.text)
-        .click(facebookClickHandler).appendTo(buttonContainer);
-    };
-
-    var addTwitterButton = function() {
-    twitterButton = $("<button />").addClass("share-twitter").text(opts.twitter.text)
-        .click(twitterClickHandler).appendTo(buttonContainer);
-    };
-
-    var addRedditButton = function() {
-    redditButton = $("<button />").addClass("share-reddit").text(opts.reddit.text)
-        .click(redditClickHandler).appendTo(buttonContainer);
-    };
-
-    var addGPlusButton = function() {
-    gplusButton = $("<button />").addClass("share-gplus").text(opts.gplus.text)
-        .click(gplusClickHandler).appendTo(buttonContainer);
-    };
-
-    var addPinterestButton = function() {
-    pinterestButton = $("<button />").addClass("share-pinterest").text(opts.pinterest.text)
-        .click(pinterestClickHandler).appendTo(buttonContainer);
-    };
-
-    var addIconClasses = function() {
-        closeButton.addClass(opts.closeButton.iconClass);
-        if (facebookButton) { facebookButton.addClass(opts.facebook.iconClass); }
-        if (twitterButton) { twitterButton.addClass(opts.twitter.iconClass); }
-        if (redditButton) { redditButton.addClass(opts.reddit.iconClass); }
-        if (gplusButton) { gplusButton.addClass(opts.gplus.iconClass); }
-        if (pinterestButton) { pinterestButton.addClass(opts.pinterest.iconClass); }
-    };
-
-    var attachStyles = function() {
-        modal.css( { "position": "fixed", "width": "100%", "height": "100%", "z-index": 9999 });
-        modal.css(opts.modal.hideCss);
-    };
-
-    var addOpenEvents = function() {
-        openButton.click(function() {
-            showModal();
-        });
-    }
-
-    var addCloseEvents = function() {
-        closeButton.click(function() {
-            hideModal();
-        });
-
-        $(document).keyup(function(e) {
-            if (e.which == 27) {
-                hideModal();
-            }
-        });
-    }
-
-    var facebookClickHandler = function() {
+    // Click Handlers
+    
+    var facebookClickHandler = function(opts) {
         var link = encodeURIComponent(opts.url);
         popupWindow('https://facebook.com/sharer/sharer.php?u=' + link);
     };
 
-    var twitterClickHandler = function() {
+    var twitterClickHandler = function(opts) {
         var link = encodeURIComponent(opts.url);
         var url = 'https://twitter.com/intent/tweet?url=' + link;
         if (opts.twitter.tweetText) { url += '&text=' + encodeURIComponent(opts.twitter.tweetText); }
@@ -154,17 +123,17 @@
         popupWindow(url);
     };
 
-    var redditClickHandler = function() {
+    var redditClickHandler = function(opts) {
         var link = encodeURIComponent(opts.url);
         window.open('https://www.reddit.com/submit?url=' + link);
     };
 
-    var gplusClickHandler = function() {
+    var gplusClickHandler = function(opts) {
         var link = encodeURIComponent(opts.url);
         popupWindow('https://plus.google.com/share?url=' + link);
     };
 
-    var pinterestClickHandler = function() {
+    var pinterestClickHandler = function(opts) {
         var link = encodeURIComponent(opts.url);
         var url = 'https://www.pinterest.com/pin/create/button?url=' + link;
         if (opts.pinterest.media) { url += '&media=' + encodeURIComponent(opts.pinterest.media); }
@@ -172,13 +141,7 @@
         popupWindow(url);
     };
 
-    var showModal = function() {
-        modal.animate(opts.modal.showCss, opts.modal.showAnimationDuration);
-    };
-
-    var hideModal = function() {
-        modal.animate(opts.modal.hideCss, opts.modal.hideAnimationDuration);
-    };
+    // Helpers
 
     var popupWindow = function(url) {
         var width = Math.min(600, window.screen.width * 0.8);
